9155af1089c4e105b9a6b3e1732198a2d6d9b208
jiayeZhu <1352953@tongji.edu.cn>
2019-11-16 02:04:54 -0500

    experiment result data

--- utils/bootClients.sh
+++ utils/bootClients.sh
@@ -1,4 +1,4 @@
 for((i = 0; i < $1; i++))
 do
-  python3 -u ClientBot.py -d cbot_$[0+$i] -M $2 -N $3 -r $4 -f $5 >client_$[0+$i].log &
+  python3 -u ClientBot.py -d cbot_$[0+$i] -M $2 -N $3 -r $4 -f $5 1>client_$[0+$i].log 2>/dev/null &
 done
